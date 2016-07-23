3d9b5808966f8d4fe4e95704eaf7946f5a3e8fc7
Vincent Léné <lene.vincent@hotmail.fr>
2016-07-23 14:26:12 +0200

    Creattion du CRUD pour UserBundle

---
+++ src/UserBundle/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('login')
+            ->add('password')
+            ->add('salt')
+            ->add('nom')
+            ->add('prenom')
+            ->add('birthDate', 'date')
+            ->add('pseudo')
+            ->add('userRoles')
+            ->add('team')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\User'
+        ));
+    }
+}
