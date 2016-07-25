993be9cf1177ef2b6473dd29ed706b43a14868bb
Vincent Léné <lene.vincent@hotmail.fr>
2016-07-25 17:58:30 +0200

    creation du crud pour teamBundle

---
+++ src/TeamBundle/Form/TeamType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TeamBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeamType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('tag')
+            ->add('victoires')
+            ->add('defaites')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TeamBundle\Entity\Team'
+        ));
+    }
+}
